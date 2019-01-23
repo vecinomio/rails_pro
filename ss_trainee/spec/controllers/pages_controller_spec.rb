0fde4a84bde855254e3f1a4859edb7351427dd71
Igor Makienko <imaki@softserveinc.com>
2019-01-23 14:42:00 +0200

    test0009

--- ss_trainee/spec/controllers/pages_controller_spec.rb
+++ ss_trainee/spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe PagesController, :type => :controller do
   it 'returns the page code status 200' do
     get :about
-    expect(response.status).to eq(401)
+    expect(response.status).to eq(422)
   end
 end
