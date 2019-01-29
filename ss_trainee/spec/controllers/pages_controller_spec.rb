626a56fa7620f768e06bd37d35fecac15e5da03d
vecinomio <37980289+vecinomio@users.noreply.github.com>
2019-01-29 17:24:17 +0200

    Update pages_controller_spec.rb

--- ss_trainee/spec/controllers/pages_controller_spec.rb
+++ ss_trainee/spec/controllers/pages_controller_spec.rb
@@ -5,6 +5,6 @@ require 'rails_helper'
 RSpec.describe PagesController, type: :controller do
   it 'returns the page code status 200' do
     get :about
-    expect(response.status).to eq(404)
+    expect(response.status).to eq(400)
   end
 end
