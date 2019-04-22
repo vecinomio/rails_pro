a59e804d724adb55bb83e25856d69c5f5251fbe9
Igor Makienko <imaki@softserveinc.com>
2019-04-22 12:39:25 +0300

    Added new description on rspec tests output

--- ss_trainee/spec/controllers/pages_controller_spec.rb
+++ ss_trainee/spec/controllers/pages_controller_spec.rb
@@ -3,14 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe PagesController, type: :controller do
-  it 'returns the page code status 200' do
+  it 'Test returns the page code status 200 from page: About us' do
     get :about
     expect(response.status).to eq(200)
   end
 end
 
 RSpec.describe PagesController, type: :controller do
-  it 'returns the page code status 200' do
+  it 'Test returns the page code status 200 from Contacts page' do
     get :contacts
     expect(response.status).to eq(200)
   end
